d26073a454e64cdb5801d59615b035bc3ba87018
ParvizAlizade <perviz.elizade.2002@mail.ru>
2023-02-10 16:14:37 +0400

    Create AllHeader and Section-1 Section-2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,53 @@
 $(document).ready(function () {
-    $(".navbar .menu #drone").hover(function(){
-        $(".onhoverdrone").css("visibility","visible");
+    $(".drone-item").mouseover(function(){
+        $(".onhoverdrone").addClass('active');
       });
-      $(".navbar .menu #drone").mouseleave(function(){
-        $(".onhoverdrone").css("visibility","hidden");
+      $(".drone-item").mouseout(function(){
+        $(".onhoverdrone").removeClass('active');
       });
 
-      $(".navbar .menu #accesories").hover(function(){
-        $(".onhoveraccesories").css("visibility","visible");
+      $(".accessories-item").mouseover(function(){
+        $(".onhoveraccesories").addClass('active');
       });
-      $(".navbar .menu #accesories").mouseleave(function(){
-        $(".onhoveraccesories").css("visibility","hidden");
-      });      
-})
+      $(".accessories-item").mouseout(function(){
+        $(".onhoveraccesories").removeClass('active');
+      });    
+
+      $('.slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        dots:true,
+        autoplay:true,
+        autoplaySpeed:3800,
+        pauseOnHover:false,
+      });;
+
+
+      $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:false
+            },
+            1000:{
+                items:2,
+                nav:true,
+                loop:false,
+                nav:false
+            }
+        }
+    })
+      $("header").mouseenter(function () {
+        $(".slick-prev, .slick-next").css("opacity",1).css("transition","0.8s")
+      })
+      $("header").mouseleave(function () {
+        $(".slick-prev, .slick-next").css("opacity",0).css("transition","0.8s")
+      })
+    });
